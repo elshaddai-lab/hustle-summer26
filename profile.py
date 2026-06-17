4a50a609d1f3e1e9ef52fdc62c4ea655786823b4
elshaddai manyahlhal <lali@elshaddais-MacBook-Air.local>
2026-06-17 10:46:26 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,12 @@
+
+full_name = "Elshaddai Manyahlhal"
+nickname = "Elshu"
+age = 17
+used_python_before = True
+hobbies = ["walking","reading","listening to music"]
+favorites = {"movie": "He's just not that into you", "food":"garlic bread", "hobby":"reading"}
+
+print(full_name, age, hobbies)
+
+skills = ("crochet","volleyball") #tuple: orderd, cannot change
+languages_spoken ={"English", "Amharic"} #set: no duplicates
